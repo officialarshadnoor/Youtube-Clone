e03f0c94136dfbbc01d6e3e15c486d5e662f00de
officialarshadnoor <arshadnoor585@gmail.com>
2023-10-24 00:59:48 +0530

    youtube clone

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Sandeep Maheshwari', icon: <CodeIcon />, },
+  { name: 'Raj Shamani', icon: <CodeIcon />, },
+  { name: 'Joe Rogan Podcast', icon: <CodeIcon />, },
+  { name: 'Artificial Intelligence', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/an_webp/TODMO0aSuDw/mqdefault_6s.webp?du=3000&sqp=CMD-2qkG&rs=AOn4CLALpH8tVPEDRQWO1Gs1vSnW6LuLng';
+export const demoChannelUrl = '/channel/SandeepSeminars';
+export const demoVideoUrl = '/video/TODMO0aSuDw';
+export const demoChannelTitle = 'Piemond Infotech';
+export const demoVideoTitle = 'A responsive Youtube Clone with ReactJS and Material UI';
+export const demoProfilePicture = 'https://media.licdn.com/dms/image/D4D03AQHlAbcDgj4Y-g/profile-displayphoto-shrink_800_800/0/1665852239455?e=2147483647&v=beta&t=vNh2W7KlxmmJ9WXpRB0UsZm1VKD4BWqC0ofKt1NfRcs'
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '39f65e1be4msh181ae88f1c05ba7p17307ejsnab88bb3f9183'
+,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
